2439c74ac8224261c5c7abaf177b2ecd1a511e4a
Sander Haug <sander_haug@hotmail.com>
2022-12-05 14:49:40 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sander Haug on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
