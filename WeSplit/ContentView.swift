1b2502ce2d81b052383f0220e3078eaf9995adb1
Sander Haug <sander_haug@hotmail.com>
2022-12-05 15:07:43 +0100

    Create_Form
    Differences with Group / Section is the visual difference that Section creates a literal section

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Test Test!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+                // Visual difference, use section
+                Section {
+                    Text("Hello World")
+                }
+            }
+            .navigationTitle("WeSplit")
+            
         }
-        .padding()
     }
 }
 
