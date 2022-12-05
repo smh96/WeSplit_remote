71f296123910536375e6c24a979c80765bbe933e
Sander Haug <sander_haug@hotmail.com>
2022-12-05 16:49:51 +0100

    Added Textfields and Picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker(selection: $numberOfPeople, label: Text("Select participants")) {
+                        ForEach(2..<26) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                }
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                }
+            }
+            .navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
